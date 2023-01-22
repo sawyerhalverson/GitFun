b30e84be603f999ab0e57365456324480ca67d66
sawyerhalverson <103144958+sawyerhalverson@users.noreply.github.com>
2023-01-21 20:33:48 -0700

    i am lit

--- GitFun/Program.cs
+++ GitFun/Program.cs
@@ -7,6 +7,7 @@ namespace GitFun
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World! I am lit.");
+            Console.WriteLine("Hi");
         }
     }
 }
